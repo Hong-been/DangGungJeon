11d51b871678e7753518a17ce9003d3090f86692
HongBeen <redbean-2@naver.com>
2021-08-24 20:01:23 +0900

    app.py에 html탬플릿 연결

--- app.py
+++ app.py
@@ -26,5 +26,10 @@ def free():
 def online():
     return render_template('online.html')
 
+# About Us 카테고리
+@app.route('/about_us')
+def aboutUs():
+    return render_template('about_us.html')
+
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
