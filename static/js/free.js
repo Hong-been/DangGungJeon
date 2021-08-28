d66f1172242013ceab504ed1d256a6d8cb75d8b9
YOOHYOJOO <p_bling@naver.com>
2021-08-28 18:49:46 +0900

    .

--- static/js/free.js
+++ static/js/free.js
@@ -1,38 +1,12 @@
-// $(document).ready(function () {
-//     showfree();
-// });
-//
-// function postfree() {
-//     let free_img = $('#exhibition-img').val()
-//     let free_name = $('#exhibition-name').val()
-//     $.ajax({
-//         type: "POST",
-//         url: "/free",
-//         data: {free_img_give: img, free_name_give: name},
-//         success: function (response) { // 성공하면
-//             alert(response["msg"]);
-//         }
-//     })
-// }
-//
-// function showfree() {
-//     $.ajax({
-//         type: "GET",
-//         url: "/free",
-//         data: {},
-//         success: function (response) {
-//             let free
-//             -respons['all_free']
-//             for (let i = 0; i < free.length; i++) {
-//                 let name = free[i]['name']
-//                 let image = free[i]['image']
-//
-//                 let temp_html = `<li class="exhibition">
-//                                     <a><img class="exhibition-img" src="${image}" /></a>
-//                                     <span class="exhibition-name">"${name}"</span>
-//                                     </li>`
-//                 $('#exhibition-list').append(temp_html)
-//             }
-//         }
-//     })
-// }
+
+function showfree() {
+    $.ajax({
+        type: "GET",
+        url: "/free",
+        data: {},
+        success: function (response) {
+            let free = respons['all_free']
+            }
+        }
+    })
+}
